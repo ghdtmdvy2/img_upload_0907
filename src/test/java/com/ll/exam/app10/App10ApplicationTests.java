7a82c94ebc2090947139cb3cdea83f053d2cab53
ghdtmdvy2 <ghdtmdvy2@gmail.com>
2022-09-13 13:20:08 +0900

    test DB 와 개발용 DB 분리

--- src/test/java/com/ll/exam/app10/App10ApplicationTests.java
+++ src/test/java/com/ll/exam/app10/App10ApplicationTests.java
@@ -1,15 +1,20 @@
 package com.ll.exam.app10;
 
 import com.ll.exam.app10.app.home.controller.HomeController;
+import com.ll.exam.app10.app.member.service.MemberService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
-import org.springframework.transaction.annotation.Transactional;
 
+import javax.transaction.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -18,25 +23,52 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @AutoConfigureMockMvc
 @Transactional
+@ActiveProfiles({"base-addi", "test"})
 class AppTests {
 
 	@Autowired
-	private MockMvc mvc; // 호출기, 브라우저이다. 특정 url 요청을 보낼 수 있다.
+	private MockMvc mvc;
+
+	@Autowired
+	private MemberService memberService;
 
 	@Test
-	@Transactional
 	@DisplayName("메인화면에서는 안녕이 나와야 한다.")
 	void t1() throws Exception {
-		// when
+		// WHEN
+		// GET /
 		ResultActions resultActions = mvc
-				.perform(get("/")) // 유저가 get 루트 페이지로 이동해라.
-				.andDo(print()); // print는 console에 출력을 해줘라.
+				.perform(get("/"))
+				.andDo(print());
 
-		// then
+		// THEN
+		// 안녕
 		resultActions
-				.andExpect(status().is2xxSuccessful()) // 200 : 성공, 300 : redirection, 400 : 클라 잘못, 500 : 서버 잘못 <- 지금은 성공
-				.andExpect(handler().handlerType(HomeController.class)) // HomeController로 갈거라고
-				.andExpect(handler().methodName("main")) // main 메서드로 갈거라고
-				.andExpect(content().string(containsString("안녕"))); // 페이지에 접속을 했을 때 "안녕"이 포함 되어있다.
+				.andExpect(status().is2xxSuccessful())
+				.andExpect(handler().handlerType(HomeController.class))
+				.andExpect(handler().methodName("main"))
+				.andExpect(content().string(containsString("안녕")));
+	}
+
+	@Test
+	@DisplayName("회원의 수")
+	@Rollback(false)
+	void t2() throws Exception {
+		long count = memberService.count();
+		assertThat(count).isGreaterThan(0);
+	}
+
+	@Test
+	@DisplayName("user1로 로그인 후 프로필페이지에 접속하면 user1의 이메일이 보여야 한다.")
+	@Rollback(false)
+	void t3() throws Exception {
+		// mockMvc로 로그인 처리
+	}
+
+	@Test
+	@DisplayName("user4로 로그인 후 프로필페이지에 접속하면 user4의 이메일이 보여야 한다.")
+	@Rollback(false)
+	void t4() throws Exception {
+		// mockMvc로 로그인 처리
 	}
 }
