140e6833efb152176a14a53aff98912dddd8b750
ghdtmdvy2 <ghdtmdvy2@gmail.com>
2022-09-16 14:50:32 +0900

    회원 정보 수정

--- src/main/java/com/ll/exam/app10/app/member/controller/MemberController.java
+++ src/main/java/com/ll/exam/app10/app/member/controller/MemberController.java
@@ -2,15 +2,16 @@ package com.ll.exam.app10.app.member.controller;
 
 import com.ll.exam.app10.app.member.entity.Member;
 import com.ll.exam.app10.app.member.service.MemberService;
+import com.ll.exam.app10.app.security.dto.MemberContext;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.CacheControl;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,7 +22,6 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.security.Principal;
 import java.util.concurrent.TimeUnit;
 
 @Controller
@@ -32,15 +32,15 @@ public class MemberController {
     private final PasswordEncoder passwordEncoder;
 
     @PreAuthorize("isAnonymous()")
-    @GetMapping("/join")
-    public String showJoin() {
-        return "member/join";
-    }
-
-    @PreAuthorize("isAnonymous()")
     @GetMapping("/login")
     public String showLogin() {
         return "member/login";
+    }
+
+    @PreAuthorize("isAnonymous()")
+    @GetMapping("/join")
+    public String showJoin() {
+        return "member/join";
     }
 
     @PreAuthorize("isAnonymous()")
@@ -67,28 +67,33 @@ public class MemberController {
     }
 
     @PreAuthorize("isAuthenticated()")
-    @GetMapping("/profile")
-    public String showProfile(Principal principal, Model model) {
-        Member loginedMember = memberService.getMemberByUsername(principal.getName());
-
-        model.addAttribute("loginedMember", loginedMember);
-
-        return "member/profile";
+    @GetMapping("/modify")
+    public String showModify() {
+        return "member/modify";
     }
 
-    @GetMapping("/profile/imgorigin/{id}")
-    public String showProfileImgOrigin(@PathVariable Long id) {
-        return "redirect:" + memberService.getMemberById(id).getProfileImgUrl();
+    @PreAuthorize("isAuthenticated()")
+    @PostMapping("/modify")
+    public String modify(@AuthenticationPrincipal MemberContext context, String email, MultipartFile profileImg) {
+        Member member = memberService.getMemberById(context.getId());
+
+        memberService.modify(member, email, profileImg);
+
+        return "redirect:/member/profile";
+    }
+
+    @PreAuthorize("isAuthenticated()")
+    @GetMapping("/profile")
+    public String showProfile() {
+        return "member/profile";
     }
 
     @GetMapping("/profile/img/{id}")
     public ResponseEntity<Object> showProfileImg(@PathVariable Long id) throws URISyntaxException {
-        System.out.println("안녕하세요.");
-
         URI redirectUri = new URI(memberService.getMemberById(id).getProfileImgUrl());
         HttpHeaders httpHeaders = new HttpHeaders();
         httpHeaders.setLocation(redirectUri);
-        httpHeaders.setCacheControl(CacheControl.maxAge(60, TimeUnit.SECONDS));
+        httpHeaders.setCacheControl(CacheControl.maxAge(60 * 60 * 1, TimeUnit.SECONDS));
         return new ResponseEntity<>(httpHeaders, HttpStatus.FOUND);
     }
 }
